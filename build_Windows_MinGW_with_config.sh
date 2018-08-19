36993fd67a57c7f59ab9d55a602b0e6c3aaebac7
NikitaFeodonit <nfeodonit@yandex.com>
2018-08-19 18:35:56 +0300

    Replace the license header text for Feographia.

--- build_Windows_MinGW_with_config.sh
+++ build_Windows_MinGW_with_config.sh
@@ -1,11 +1,11 @@
 # ****************************************************************************
-#  Project:  HtmlGrapheas
-#  Purpose:  HTML text editor library
+#  Project:  Feographia
+#  Purpose:  The application to work with the biblical text
 #  Author:   NikitaFeodonit, nfeodonit@yandex.com
 # ****************************************************************************
 #    Copyright (c) 2017-2018 NikitaFeodonit
 #
-#    This file is part of the HtmlGrapheas project.
+#    This file is part of the Feographia project.
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published

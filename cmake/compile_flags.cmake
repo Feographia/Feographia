64a63231bcbb34dd593b613332e75ab009b0edcf
NikitaFeodonit <nfeodonit@yandex.com>
2020-10-19 10:38:48 +0300

    CMake: Add MSVC compiler warnings

--- cmake/compile_flags.cmake
+++ cmake/compile_flags.cmake
@@ -165,12 +165,140 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang"
     -Wgnu -Wmicrosoft
 
     # -Wno-*
-    -Wno-unknown-warning-option
+    -Wno-unknown-pragmas -Wno-unknown-warning-option
   )
 endif()
 
 if(MSVC)
-  add_compile_options(/WX /Wall)
+  add_compile_options(
+    /WX /W4 /permissive-
+
+    # Compiler warnings that are off by default
+    -we4061 # (level 4)  enumerator 'identifier' in a switch of enum 'enumeration' is not explicitly handled by a case label
+    -we4062 # (level 4)  enumerator 'identifier' in a switch of enum 'enumeration' is not handled
+    -we4165 # (level 1)  'HRESULT' is being converted to 'bool'; are you sure this is what you want?
+    -we4191 # (level 3)  'operator': unsafe conversion from 'type_of_expression' to 'type_required'
+    -we4242 # (level 4)  'identifier': conversion from 'type1' to 'type2', possible loss of data
+    -we4254 # (level 4)  'operator': conversion from 'type1' to 'type2', possible loss of data
+    -we4255 # (level 4)  'function': no function prototype given: converting '()' to '(void)'
+    -we4263 # (level 4)  'function': member function does not override any base class virtual member function
+    -we4264 # (level 1)  'virtual_function': no override available for virtual member function from base 'class'; function is hidden
+    -we4265 # (level 3)  'class': class has virtual functions, but destructor is not virtual
+    -we4266 # (level 4)  'function': no override available for virtual member function from base 'type'; function is hidden
+    -we4287 # (level 3)  'operator': unsigned/negative constant mismatch
+    -we4289 # (level 4)  nonstandard extension used : 'var' : loop control variable declared in the for-loop is used outside the for-loop scope
+    -we4296 # (level 4)  'operator': expression is always false
+    -we4339 # (level 4)  'type' : use of undefined type detected in CLR meta-data - use of this type may lead to a runtime exception
+    -we4342 # (level 1)  behavior change: 'function' called, but a member operator was called in previous versions
+    -we4350 # (level 1)  behavior change: 'member1' called instead of 'member2'
+    -we4355 #            'this' : used in base member initializer list
+# System headers
+#    -we4365 # (level 4)  'action': conversion from 'type_1' to 'type_2', signed/unsigned mismatch
+    -we4370 # (level 3)  layout of class has changed from a previous version of the compiler due to better packing
+    -we4371 # (level 3)  'classname': layout of class may have changed from a previous version of the compiler due to better packing of member 'member'
+# GTest
+#    -we4388 # (level 4)  signed/unsigned mismatch
+    -we4412 # (level 2)  'function': function signature contains type 'type'; C++ objects are unsafe to pass between pure code and mixed or native
+    -we4426 # (level 1)  optimization flags changed after including header, may be due to #pragma optimize() 14.1
+    -we4435 # (level 4)  'class1' : Object layout under /vd2 will change due to virtual base 'class2'
+    -we4437 # (level 4)  dynamic_cast from virtual base 'class1' to 'class2' could fail in some contexts
+    -we4444 # (level 3)  top level '__unaligned' is not implemented in this context
+    -we4464 # (level 4)  relative include path contains '..'
+    -we4471 # (level 4)  a forward declaration of an unscoped enumeration must have an underlying type (int assumed) Perm
+    -we4472 # (level 1)  'identifier' is a native enum: add an access specifier (private/public) to declare a managed enum
+# System headers
+#    -we4514 # (level 4)  'function': unreferenced inline function has been removed
+    -we4536 # (level 4)  'type name': type-name exceeds meta-data limit of 'limit' characters
+    -we4545 # (level 1)  expression before comma evaluates to a function which is missing an argument list
+    -we4546 # (level 1)  function call before comma missing argument list
+    -we4547 # (level 1)  'operator': operator before comma has no effect; expected operator with side-effect
+# System headers
+#    -we4548 # (level 1)  expression before comma has no effect; expected expression with side-effect
+    -we4549 # (level 1)  'operator1': operator before comma has no effect; did you intend 'operator2'?
+    -we4555 # (level 1)  expression has no effect; expected expression with side-effect
+    -we4557 # (level 3)  '__assume' contains effect 'effect'
+# System headers
+#    -we4571 # (level 4)  informational: catch(...) semantics changed since Visual C++ 7.1; structured exceptions (SEH) are no longer caught
+    -we4574 # (level 4)  'identifier' is defined to be '0': did you mean to use '#if identifier'?
+    -we4577 # (level 1)  'noexcept' used with no exception handling mode specified; termination on exception is not guaranteed. Specify /EHsc
+    -we4582 # (level 4)  'type': constructor is not implicitly called
+    -we4583 # (level 4)  'type': destructor is not implicitly called
+    -we4587 # (level 1)  'anonymous_structure': behavior change: constructor is no longer implicitly called
+    -we4588 # (level 1)  'anonymous_structure': behavior change: destructor is no longer implicitly called
+    -we4596 # (level 4)  'identifier': illegal qualified name in member declaration 14.3 Perm
+    -we4598 # (level 1 and level 3)  '#include "header"': header number header-number in the precompiled header does not match current compilation at that position 14.3
+    -we4599 # (level 3)  'option path': command-line argument number number does not match pre-compiled header 14.3
+    -we4605 # (level 1)  '/Dmacro' specified on current command line, but was not specified when precompiled header was built
+    -we4608 # (level 3)  'union_member' has already been initialized by another union member in the initializer list, 'union_member' Perm
+    -we4619 # (level 3)  #pragma warning: there is no warning number 'number'
+# System headers
+#    -we4623 # (level 4)  'derived class': default constructor could not be generated because a base class default constructor is inaccessible
+# System headers
+#    -we4625 # (level 4)  'derived class': copy constructor could not be generated because a base class copy constructor is inaccessible
+# System headers
+#    -we4626 # (level 4)  'derived class': assignment operator could not be generated because a base class assignment operator is inaccessible
+    -we4628 # (level 1)  digraphs not supported with -Ze. Character sequence 'digraph' not interpreted as alternate token for 'char'
+    -we4640 # (level 3)  'instance': construction of local static object is not thread-safe
+    -we4643 # (level 4)  Forward declaring 'identifier' in namespace std is not permitted by the C++ Standard. 15.8
+    -we4647 # (level 3)  behavior change: __is_pod(type) has different value in previous versions
+    -we4654 # (level 4)  Code placed before include of precompiled header line will be ignored. Add code to precompiled header. 14.1
+# GTest
+#    -we4668 # (level 4)  'symbol' is not defined as a preprocessor macro, replacing with '0' for 'directives'
+    -we4682 # (level 4)  'symbol' : no directional parameter attribute specified, defaulting to [in]
+    -we4686 # (level 3)  'user-defined type': possible change in behavior, change in UDT return calling convention
+    -we4692 # (level 1)  'function': signature of non-private member contains assembly private native type 'native_type'
+# System headers
+#    -we4710 # (level 4)  'function': function not inlined
+    -we4738 # (level 3)  storing 32-bit float result in memory, possible loss of performance
+    -we4746 #            volatile access of 'expression' is subject to /volatile:<iso|ms> setting; consider using __iso_volatile_load/store intrinsic functions
+    -we4749 # (level 4)  conditionally supported: offsetof applied to non-standard-layout type 'type'
+    -we4767 # (level 4)  section name 'symbol' is longer than 8 characters and will be truncated by the linker
+    -we4768 # (level 3)  __declspec attributes before linkage specification are ignored
+# System headers
+#    -we4774 # (level 4)  'string' : format string expected in argument number is not a string literal
+    -we4777 # (level 4)  'function' : format string 'string' requires an argument of type 'type1', but variadic argument number has type 'type2'
+    -we4786 # (level 3)  'symbol' : object name was truncated to 'number' characters in the debug information
+    -we4800 # (level 4)  Implicit conversion from 'type' to bool. Possible information loss 16.0
+# System headers
+#    -we4820 # (level 4)  'bytes' bytes padding added after construct 'member_name'
+    -we4822 # (level 1)  'member': local class member function does not have a body
+    -we4826 # (level 2)  Conversion from 'type1' to 'type2' is sign-extended. This may cause unexpected runtime behavior.
+    -we4837 # (level 4)  trigraph detected: '??character' replaced by 'character'
+    -we4841 # (level 4)  non-standard extension used: compound member designator used in offsetof
+    -we4842 # (level 4)  the result of 'offsetof' applied to a type using multiple inheritance is not guaranteed to be consistent between compiler releases
+    -we4868 # (level 4)  'file(line_number)' compiler may not enforce left-to-right evaluation order in braced initialization list
+    -we4905 # (level 1)  wide string literal cast to 'LPSTR'
+    -we4906 # (level 1)  string literal cast to 'LPWSTR'
+    -we4917 # (level 1)  'declarator': a GUID can only be associated with a class, interface, or namespace
+    -we4928 # (level 1)  illegal copy-initialization; more than one user-defined conversion has been implicitly applied
+    -we4931 # (level 4)  we are assuming the type library was built for number-bit pointers
+    -we4946 # (level 1)  reinterpret_cast used between related classes: 'class1' and 'class2'
+    -we4962 #            'function': profile-guided optimizations disabled because optimizations caused profile data to become inconsistent
+    -we4986 # (level 4)  'symbol': exception specification does not match previous declaration
+    -we4987 # (level 4)  nonstandard extension used: 'throw (...)'
+    -we4988 # (level 4)  'symbol': variable declared outside class/function scope
+    -we5022 #            'type': multiple move constructors specified
+    -we5023 #            'type': multiple move assignment operators specified
+    -we5024 # (level 4)  'type': move constructor was implicitly defined as deleted
+    -we5025 # (level 4)  'type': move assignment operator was implicitly defined as deleted
+# System headers
+#    -we5026 # (level 1 and level 4)  'type': move constructor was implicitly defined as deleted
+# System headers
+#    -we5027 # (level 1 and level 4)  'type': move assignment operator was implicitly defined as deleted
+    -we5029 # (level 4)  nonstandard extension used: alignment attributes in C++ apply to variables, data members and tag types only
+    -we5031 # (level 4)  #pragma warning(pop): likely mismatch, popping warning state pushed in different file 14.1
+    -we5032 # (level 4)  detected #pragma warning(push) with no corresponding #pragma warning(pop) 14.1
+    -we5034 #            use of intrinsic 'intrinsic' causes function function-name to be compiled as guest code 15.3
+    -we5035 #            use of feature 'feature' causes function function-name to be compiled as guest code 15.3
+    -we5036 # (level 1)  varargs function pointer conversion when compiling with /hybrid:x86arm64 'type1' to 'type2' 15.3
+    -we5038 # (level 4)  data member 'member1' will be initialized after data member 'member2' 15.3
+    -we5039 # (level 4)  'function': pointer or reference to potentially throwing function passed to extern C function under -EHc. Undefined behavior may occur if this function throws an exception. 15.5
+    -we5042 # (level 3)  'function': function declarations at block scope cannot be specified 'inline' in standard C++; remove 'inline' specifier 15.5
+    -we5045 #            Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified 15.7
+
+    # Suppress warnings.
+    -wd4068 # (level 1) unknown pragma
+  )
 endif()
 
 option(cmr_BUILD_MULTIPROC "cmr_BUILD_MULTIPROC" ON)
